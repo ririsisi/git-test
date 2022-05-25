63e4766ff99d4a6b0f536a50175b6e0d82f0cb9b
niqi <syq278872032@gmail.com>
2022-05-25 11:21:53 +0800

    first commit

---
+++ src/main/java/com/ririsisi/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.ririsisi.git;
+
+/**
+ * @Author songNi
+ * @Version 1.0
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
